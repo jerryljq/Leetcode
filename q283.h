2f1fc57867e4da5a88f8f416e6057487f4e21f20
Jiaqi Li <kapiservice@sina.com>
2016-09-21 10:26:08 -0700

    add q283

---
+++ q283.h
@@ -0,0 +1,32 @@
+//
+// Created by Jerry Li on 2016/9/20.
+//
+/********************* Problem Specification *********************
+ Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+ Note: You must do this in-place without making a copy of the array. Minimize the total number of operations.
+ ***************************** Solution **************************
+ The results do not come randomly. Find the periodically rule.
+ ******************************** End ****************************
+*/
+#ifndef LEETCODE_Q283_H
+#define LEETCODE_Q283_H
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int numberOfZero = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 0) {
+                nums.erase(nums.begin() + i);
+                i-=1;
+                numberOfZero += 1;
+            }
+        }
+        for(int j = 0; j < numberOfZero; j++) {
+            nums.push_back(0);
+        }
+    }
+};
+
+#endif //LEETCODE_Q283_H
